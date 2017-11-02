c387c43dd428e310ba364dd8eff9c706a656fceb
suhan1996 <hs2657@nyu.edu>
2017-11-02 16:26:08 +0400

    test

--- routes/index.js
+++ routes/index.js
@@ -9,9 +9,7 @@ var url = "mongodb://suhan1996:112496@ds227555.mlab.com:27555/webber";
 
 
 
-router.get('/database', function(req, res, next) {
-    res.render('index', { title: 'Express' });
-});
+
 
 router.get('/', function(req, res, next) {
 
@@ -26,6 +24,27 @@ router.get('/', function(req, res, next) {
             res.send(result);
         });
     });
+});
+
+router.get('/:title', function(req, res, next) {
+    var title = req.params.title;
+
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        //var query = { address: "Park Lane 38" };
+        var query = {};
+        db.collection("boom").find({title:title}).toArray(function(err, result) {
+            if (err) throw err;
+            console.log(result);
+            db.close();
+            res.send(result);
+        });
+    });
+});
+
+
+router.get('/database', function(req, res, next) {
+    res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
